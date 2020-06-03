12146d8b272f0e76fed8620adc21f7db6b27b659
Dam Phan <dam.phan@adurolife.com>
2020-06-03 18:05:56 +0700

    Init source

--- README.md
+++ README.md
@@ -1,24 +1,121 @@
-# triangledspiderchart
+# Vue.js Triangled Spider Chart
+A Vue.js component that displays a triangled style spider chart.
+![Full options with 1 fill color](./resources/full_options_1.png =200x200)
 
-## Project setup
+## Simple setup
 ```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
+npm install triangled-spider-chart
 ```
 
-### Compiles and minifies for production
+## Simple usage
+In the script
 ```
-npm run build
-```
+import TriangledSpiderChart from "triangled-spider-chart";
 
-### Lints and fixes files
+export default {
+  ...
+  components: {
+    TriangledSpiderChart
+  },
+  data() {
+    return {
+      chartData: [5, 10, 15, 20, 25, 30, 35]
+    }
+  }
+}
 ```
-npm run lint
+In the template
+```
+<TriangledSpiderChart
+  :chartData="chartData"
+></TriangledSpiderChart>
 ```
 
 ### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+#### Properties
+- **size**: *Number* Dimension (for both width and height) of the chart. Default is 300.
+- **max**: *Number* Maximum value to display within the chart. Maximum value of **chartData** if omitted.
+- **chartData**: *Array|required* Chart series data. Ex: [5, 10, 15, 20, 25, 30, 35]
+- **options** *Object* Custom styling for the chart.
+
+##### The options definition
+- **strokeWidth**: *Number* Size of the chart serie stroke. Default is 3.
+- **strokeColor**: *Number* Color fo the chart serie stroke. Default is White.
+- **fills** *String|Array* Fill colors of chart series. Ramdomize colors if omitted.
+- **hoverFills** *String|Array* Fill colors of chart series when mouse over. Opacity to 75% if omitted.
+
+## Full options example
+In the script
+```
+import TriangledSpiderChart from "triangled-spider-chart";
+
+export default {
+  ...
+  components: {
+    TriangledSpiderChart
+  },
+  data() {
+    return {
+      chartSize: 500,
+      chartMax: 35,
+      chartData: [5, 10, 15, 20, 25, 30],
+      chartOptions: {
+        strokeWidth: 5,
+        strokeColor: '#000000',
+        fills: ['#25C1C3', '#6CB91A', '#F7BA1B', '#E24CE5', '#C378FB', '#1674CD'],
+        hoverFills: ['#25C1C3BF', '#6CB91ABF', '#F7BA1BBF', '#E24CE5BF', '#C378FBBF', '#1674CDBF']
+      }
+    }
+  }
+}
+```
+In the template
+```
+<TriangledSpiderChart
+  :size="chartSize"
+  :max="chartMax"
+  :chartData="chartData"
+  :options="chartOptions"
+></TriangledSpiderChart>
+```
+
+#### Result
+![Full options with 1 fill color](./resources/full_options_1.png =200x200)
+
+## Full options example (1 fill color)
+In the script
+```
+import TriangledSpiderChart from "triangled-spider-chart";
+
+export default {
+  ...
+  components: {
+    TriangledSpiderChart
+  },
+  data() {
+    return {
+      chartSize: 500,
+      chartMax: 30,
+      chartData: [5, 10, 15, 20, 25, 30, 35],
+      chartOptions: {
+        strokeWidth: 5,
+        strokeColor: '#000000',
+        fills: '#ff0000', // all series will be fill Red.
+        hoverFills: '#00ff00' // all series will be fill Green when mouse over.
+      }
+    }
+  }
+}
+```
+In the template
+```
+<TriangledSpiderChart
+  :size="chartSize"
+  :max="chartMax"
+  :chartData="chartData"
+  :options="chartOptions"
+></TriangledSpiderChart>
+```
+
+#### Result
+![Full options with 1 fill color](./resources/full_options_1_a.png =200x200)
---
+++ src/components/TriangledSpiderChart.vue
@@ -0,0 +1,228 @@
+<template>
+  <div class="tsc-container">
+    <svg
+      preserveAspectRatio="xMidYMid meet"
+      :height="size"
+      :width="size"
+    >
+      <g
+        :transform="rotation"
+        :width="size"
+        :height="size"
+      >
+        <!-- +250 +250 -->
+        <polygon
+          v-for="index in itemCount"
+          :key="index"
+          :id="'item' + index"
+          :class="'p' + (index - 1)"
+          :points="coordString(index - 1)"
+          :style="pStyle(index - 1)"
+          @mouseover="hoverings = index - 1; onMouseOver(index - 1)"
+          @mouseout="hoverings = -1; onMouseOut(index - 1)"
+          @click="onItemClick(index - 1)"
+        >
+          <title>Value: {{ chartData[index - 1] }}</title>
+        </polygon>
+        Sorry, your browser does not support inline SVG.
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+// import { EventBus } from '../../../event-bus';
+export default {
+  name: 'triangled-spider-chart',
+  props: {
+    size: {
+      type: Number,
+      default: 300
+    },
+    max: {
+      type: Number,
+      default: null
+    },
+    chartData: {
+      type: Array,
+      required: true
+    },
+    options: {
+      type: Object,
+      default: null
+    }
+  },
+  data () {
+    return {
+      hoverings: -1,
+      selected: -1,
+      default: {
+        strokeWidth: 3,
+        strokeColor: 'white'
+      }
+    };
+  },
+  computed: {
+    center () {
+      return this.size / 2.0;
+    },
+    rotation () {
+      return 'rotate(-90 ' + this.size / 2 + ' ' + this.size / 2 + ')';
+    },
+    maxValue () {
+      return !this.max ? Math.max.apply(Math, this.chartData) : this.max;
+    },
+    angle () {
+      return (360 / this.itemCount) * (Math.PI / 180);
+    },
+    values () {
+      let values = [];
+      for (let i = 0; i < this.chartData.length; i++) {
+        values[i] = this.chartData[i] * this.center / this.maxValue;
+      }
+      return values;
+    },
+    theOptions () {
+      return !this.options ? {} : this.options;
+    },
+    coords () {
+      let coords = [];
+      for (let i = 0; i < this.itemCount; i++) {
+        let coord = [];
+        coord.push([0 + this.center, 0 + this.center]);
+        coord.push(this.getCoord(this.values[i], i * this.angle));
+        coord.push(this.getCoord(this.values[i], (i + 1) * this.angle));
+        coords[i] = coord;
+      }
+      return coords;
+    },
+    itemCount () {
+      return this.chartData.length;
+    },
+    defaultFills () {
+      let colors = [];
+      let fillsIsArray = Array.isArray(this.theOptions.fills || null);
+      let fillsIsString = this.theOptions.fills ? typeof this.theOptions.fills === 'string' : false;
+      for (let i = 0; i < this.itemCount; i++) {
+        if (fillsIsString) {
+          colors.push(this.theOptions.fills);
+        } else if (fillsIsArray && this.theOptions.fills[i]) {
+          colors.push(this.theOptions.fills[i]);
+        } else {
+          colors.push(this.getRandomColor());
+        }
+      }
+      return colors;
+    },
+    defaultHovers () {
+      let colors = [];
+      let fillsIsArray = Array.isArray(this.theOptions.hoverFills || null);
+      let fillsIsString = this.theOptions.hoverFills ? typeof this.theOptions.hoverFills === 'string' : false;
+      for (let i = 0; i < this.itemCount; i++) {
+        if (fillsIsString) {
+          colors.push(this.theOptions.hoverFills);
+        } else if (fillsIsArray && this.theOptions.hoverFills[i]) {
+          colors.push(this.theOptions.hoverFills[i]);
+        } else {
+          colors.push(0.75);
+        }
+      }
+      return colors;
+    }
+  },
+  methods: {
+    getCoord (len, angle) {
+      return [this.center + len * Math.cos(angle), this.center + len * Math.sin(angle)];
+    },
+    pStyle (index) {
+      let fill = this.hoverings === index || this.selected === index
+        ? (typeof this.defaultHovers[index] === 'number' ? this.defaultFills[index] : this.defaultHovers[index])
+        : this.defaultFills[index];
+      let strokeColor = this.theOptions.strokeColor || this.default.strokeColor;
+      let strokeWidth = this.theOptions.strokeWidth || this.default.strokeWidth;
+      let alpha = this.hoverings === index || this.selected === index
+        ? (typeof this.defaultHovers[index] === 'number' ? this.defaultHovers[index] : 1)
+        : 1;
+      return 'fill:' + fill + ';stroke:' + strokeColor + ';stroke-width:' + strokeWidth + ';opacity:' + alpha;
+    },
+    onMouseOver (index) {
+      index;
+      // EventBus.$emit('onItemMouseOver', index);
+    },
+    onMouseOut (index) {
+      index;
+      // EventBus.$emit('mouseOverOnDomainFinacial');
+    },
+    onItemClick (index) {
+      if (this.selected === index) {
+        this.setSelected(-1);
+      } else {
+        this.setSelected(index);
+      }
+      // EventBus.$emit('onItemClick', index);
+    },
+    setSelected (index) {
+      this.selected = index;
+    },
+    coordString (index) {
+      if (this.coords[index]) {
+        return this.coords[index].join(' ');
+      }
+      return '';
+    },
+    getRandomColor () {
+      return '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1,6);
+    }
+  },
+  mounted () {
+    // EventBus.$on('mouseOverOnIndexDomain', (domainType) => {
+    //   switch (domainType) {
+    //     case 'HAPPINESS_AND_LIFE_SATISFACTION':
+    //       this.hoverings = 0;
+    //       break;
+    //     case 'CHARACTER_AND_VIRTUNE':
+    //       this.hoverings = 1;
+    //       break;
+    //     case 'MENTAL_AND_PHYSICAL_HEALTH':
+    //       this.hoverings = 2;
+    //       break;
+    //     case 'CLOSE_SOCIAL_RELATIONSHIPS':
+    //       this.hoverings = 3;
+    //       break;
+    //     case 'FINANCIAL_AND_MATERIAL_STABILITY':
+    //       this.hoverings = 4;
+    //       break;
+    //     case 'MEANING_AND_PURPOSE':
+    //       this.hoverings = 5;
+    //       break;
+    //   }
+    // });
+    // EventBus.$on('mouseOutOnIndexDomain', () => {
+    //   this.hoverings = -1;
+    // });
+    // // handle click on domain
+    // EventBus.$on('on-click-domain', (domainType) => {
+    //   switch (domainType) {
+    //     case 'HAPPINESS_AND_LIFE_SATISFACTION':
+    //       this.selected = 0;
+    //       break;
+    //     case 'CHARACTER_AND_VIRTUNE':
+    //       this.selected = 1;
+    //       break;
+    //     case 'MENTAL_AND_PHYSICAL_HEALTH':
+    //       this.selected = 2;
+    //       break;
+    //     case 'CLOSE_SOCIAL_RELATIONSHIPS':
+    //       this.selected = 3;
+    //       break;
+    //     case 'FINANCIAL_AND_MATERIAL_STABILITY':
+    //       this.selected = 4;
+    //       break;
+    //     case 'MEANING_AND_PURPOSE':
+    //       this.selected = 5;
+    //       break;
+    //   }
+    // });
+  }
+};
+</script>

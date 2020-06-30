24284ff25e6f160350f53905822a3d66e52a5d3a
Dam Phan <dam.phan@adurolife.com>
2020-06-30 14:52:54 +0700

    Update version 0.1.4

--- README.md
+++ README.md
@@ -36,7 +36,8 @@ In the template
 #### Properties
 - **size**: *Number* Dimension (for both width and height) of the chart. Default is 300.
 - **max**: *Number* Maximum value to display within the chart. Maximum value of **chartData** if omitted.
-- **chartData**: *Array|required* Chart series data. Ex: [5, 10, 15, 20, 25, 30, 35]
+- **chartData**: *Array|required* Chart series data. Data can be numbers only, ex: [5, 10, 15, 20, 25, 30, 35]. Or can define as array of Object with value and label, ex: chartData: [{ value: 500, label: 'Food', fill: '#25C1C3', hover: '#25C1C3BF' }, { value: 1500, label: 'Drink' }, ...].
+- **chartLabelData** *Array* Chart series label.
 - **selectedIndex**: *Number* Default selected (highlighted) item index.
 - **options** *Object* Custom styling for the chart.
 
@@ -45,6 +46,14 @@ In the template
 - **strokeColor**: *Number* Color fo the chart serie stroke. Default is White.
 - **fills** *String|Array* Fill colors of chart series. Ramdomize colors if omitted.
 - **hoverFills** *String|Array* Fill colors of chart series when mouse over. Opacity to 75% if omitted.
+- **tooltip** *String* Display tooltip format. Use parameter ${label} and ${value} for data binding.
+- **showLegends** *Boolean* Show or hide legends. Default is *false*.
+- **showGuideLines** *Boolean* Show or hide guide lines. Default is *false*.
+- **guideColor** *String* Guide line color. Default is *#656565*.
+- **showValues** *Boolean* Show or hide data value. Default is *false*.
+- **valueColor** *String* Color of the data value. Default is *#000000*.
+- **valueStroke** *String* Stroke color of the data value. Default is *#FFFFFF*.
+- **numberFormat** *String* Number formatting fo the data value. Refer to [Numeraljs](http://numeraljs.com/#format).
 
 ### Events
 - **onItemMouseOver(index)**: Event emitted when move mouse over an item.
@@ -64,13 +73,28 @@ export default {
   data() {
     return {
       chartSize: 500,
-      chartMax: 35,
-      chartData: [5, 10, 15, 20, 25, 30],
+      chartMax: 3500,
+      chartData: [
+        { value: 500, label: 'Food', fill: '#25C1C3', hover: '#25C1C3BF'},
+        { value: 1500, label: 'Drink' },
+        { value: 2500, label: 'Clothes' },
+        { value: 3000, label: 'Transport' },
+        { value: 2000, label: 'Learning' },
+        { value: 2900, label: 'Others' }
+      ],
       chartOptions: {
-        strokeWidth: 5,
-        strokeColor: '#000000',
-        fills: ['#25C1C3', '#6CB91A', '#F7BA1B', '#E24CE5', '#C378FB', '#1674CD'],
-        hoverFills: ['#25C1C3BF', '#6CB91ABF', '#F7BA1BBF', '#E24CE5BF', '#C378FBBF', '#1674CDBF']
+        strokeWidth: 3,
+        strokeColor: '#FFFFFF',
+        fills: ['#FF0000', '#6CB91A', '#F7BA1B', '#E24CE5', '#C378FB', '#1674CD'],
+        hoverFills: ['#FF0000', '#6CB91ABF', '#F7BA1BBF', '#E24CE5BF', '#C378FBBF', '#1674CDBF'],
+        tooltip: '${label}: ${value}k',
+        showLegends: true,
+        showGuideLines: true,
+        guideColor: '#656565',
+        showValues: true,
+        valueColor: '#000',
+        valueStroke: '#FFF',
+        numberFormat: '$0,0'
       }
     }
   }
@@ -80,9 +104,9 @@ In the template
 ```
 <TriangledSpiderChart
   :size="chartSize"
-  :max="chartMax"
   :chartData="chartData"
   :options="chartOptions"
+  :max="chartMax"
 ></TriangledSpiderChart>
 ```
 
@@ -104,11 +128,13 @@ export default {
       chartSize: 500,
       chartMax: 30,
       chartData: [5, 10, 15, 20, 25, 30, 35],
+      chartLabelData: ['Food', 'Drink', 'Clothes', 'Transport', 'Learning', 'Others'],
       chartOptions: {
         strokeWidth: 5,
         strokeColor: '#000000',
         fills: '#ff0000', // all series will be fill Red.
-        hoverFills: '#00ff00' // all series will be fill Green when mouse over.
+        hoverFills: '#00ff00', // all series will be fill Green when mouse over.
+        showLegends: true
       }
     }
   }
@@ -118,9 +144,11 @@ In the template
 ```
 <TriangledSpiderChart
   :size="chartSize"
-  :max="chartMax"
   :chartData="chartData"
   :options="chartOptions"
+  :max="chartMax"
+  :chartLabelData="chartLabelData"
+  :showLegends="showLegends"
 ></TriangledSpiderChart>
 ```
 
--- src/components/TriangledSpiderChart.vue
+++ src/components/TriangledSpiderChart.vue
@@ -10,26 +10,94 @@
         :width="size"
         :height="size"
       >
-        <polygon
-          v-for="index in itemCount"
-          :key="index"
-          :id="'item' + index"
-          :class="'p' + (index - 1)"
-          :points="coordString(index - 1)"
-          :style="pStyle(index - 1)"
-          @mouseover="hoverings = index - 1; onMouseOver(index - 1)"
-          @mouseout="hoverings = -1; onMouseOut(index - 1)"
-          @click="onItemClick(index - 1)"
+        <g>
+          <polygon
+            v-for="index in itemCount"
+            :key="index"
+            :id="'item' + index"
+            :class="'p' + (index - 1)"
+            :points="coordString(index - 1)"
+            :style="pStyle(index - 1)"
+            @mouseover="hoverings = index - 1; onMouseOver(index - 1)"
+            @mouseout="hoverings = -1; onMouseOut(index - 1)"
+            @click="onItemClick(index - 1)"
+          >
+            <title>{{ tooltipData[index - 1] }}</title>
+          </polygon>
+        </g>
+        <g
+          v-if="showGuideLines"
         >
-          <title>Value: {{ chartData[index - 1] }}</title>
-        </polygon>
+          <polyline
+            v-for="(guideLine, idx) in guideLines"
+            :key="'g' + idx"
+            :points="guideLine.join(' ')"
+            class="tsc-guide-line"
+            :style="{ stroke: guideColor }"
+            @mouseover="onGuideLineMouseOver"
+            @mouseout="onGuideLineMouseOut"
+          ></polyline>
+        </g>
+        <g
+          v-if="showValues"
+        >
+          <text
+            v-for="(item, index) in textsPosition"
+            :key="'t' + index"
+            :x="item[0]"
+            :y="item[1]"
+            text-anchor="middle"
+            dominant-baseline="middle"
+            :fill="valueColor"
+            :style="{
+              transform: 'rotate(90deg)',
+              transformOrigin: item[0] + 'px ' + item[1] + 'px',
+              pointerEvents: 'none',
+              fontSize: '0.95em',
+              fontWeight: 'bold',
+              stroke: valueStroke
+            }"
+          >{{ getFormattedValue(theData[index]) }}</text>
+        </g>
+        <g v-if="showGuideLines">
+          <text
+            v-for="gVal in guideValues"
+            :key="'v' + gVal.id"
+            :x="gVal.coord[0]"
+            :y="gVal.coord[1] - 3"
+            text-anchor="middle"
+            dominant-baseline="top"
+            :fill="guideColor"
+            :style="{
+              transform: 'rotate(' + (90 + angleDegree / 2) + 'deg)',
+              transformOrigin: gVal.coord[0] + 'px ' + gVal.coord[1] + 'px',
+              opacity: showGuideValue ? 0.5 : 0.1,
+              fontWeight: 400,
+              fontSize: '0.8em',
+              cursor: 'default'
+            }"
+            @mouseover="onGuideLineMouseOver"
+            @mouseout="onGuideLineMouseOut"
+          >{{ gVal.value }}</text>
+        </g>
         Sorry, your browser does not support inline SVG.
       </g>
     </svg>
+    <div class="tsc-legends" v-if="showLegends">
+      <div
+        class="tsc-legend"
+        v-for="index in itemCount"
+        :key="index"
+      >
+        <div class="tsc-shape" :style="{ backgroundColor: defaultFills[index - 1] }"></div>
+        <div class="tsc-title">{{ labelData[index - 1] + (showValues ? ' (' + getFormattedValue(theData[index - 1]) + ')' : '') }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+const numeral = require('numeral');
 export default {
   name: 'triangled-spider-chart',
   props: {
@@ -45,6 +113,10 @@ export default {
       type: Array,
       required: true
     },
+    chartLabelData: {
+      type: Array,
+      default: null
+    },
     options: {
       type: Object,
       default: null
@@ -56,15 +128,41 @@ export default {
   },
   data () {
     return {
+      showGuideValue: false,
       hoverings: -1,
       selected: -1,
       default: {
         strokeWidth: 3,
-        strokeColor: 'white'
+        strokeColor: 'white',
+        tooltip: '${label}: ${value}',
+        guideColor: '#656565',
+        valueColor: '#000000',
+        valueStroke: '#FFFFFF'
       }
     };
   },
   computed: {
+    numberFormat () {
+      return this.options && this.options.numberFormat ? this.options.numberFormat : false;
+    },
+    showLegends () {
+      return this.options && this.options.showLegends ? true : false;
+    },
+    showGuideLines () {
+      return this.options && this.options.showGuideLines ? true : false;
+    },
+    showValues () {
+      return this.options && this.options.showValues ? true : false;
+    },
+    guideColor () {
+      return this.options && this.options.guideColor ? this.options.guideColor : this.default.guideColor;
+    },
+    valueColor () {
+      return this.options && this.options.valueColor ? this.options.valueColor : this.default.valueColor;
+    },
+    valueStroke () {
+      return this.options && this.options.valueStroke ? this.options.valueStroke : this.default.valueStroke;
+    },
     center () {
       return this.size / 2.0;
     },
@@ -72,17 +170,64 @@ export default {
       return 'rotate(-90 ' + this.size / 2 + ' ' + this.size / 2 + ')';
     },
     maxValue () {
-      return !this.max ? Math.max.apply(Math, this.chartData) : this.max;
+      return !this.max ? Math.max.apply(Math, this.theData) : this.max;
+    },
+    angleDegree () {
+      return 360 / this.itemCount;
     },
     angle () {
-      return (360 / this.itemCount) * (Math.PI / 180);
+      return this.angleDegree * (Math.PI / 180);
+    },
+    theData () {
+      let data = [];
+      for (let i = 0; i < this.chartData.length; i++) {
+        if (this.chartData[i] instanceof Object) {
+          data.push(this.chartData[i].value);
+        } else {
+          data.push(this.chartData[i]);
+        }
+      }
+      return data;
+    },
+    labelData () {
+      let labels = [];
+      let unamedCounter = 0;
+      if (this.chartLabelData && Array.isArray(this.chartLabelData)) {
+        for (let i = 0; i < this.itemCount; i++) {
+          if (this.chartLabelData[i]) {
+            labels.push(this.chartLabelData[i]);
+          } else {
+            labels.push('Unnamed ' + unamedCounter++);
+          }
+        }
+      } else {
+        for (let i = 0; i < this.itemCount; i++) {
+          labels.push('Unnamed ' + unamedCounter++);
+        }
+      }
+      for (let i = 0; i < this.chartData.length; i++) {
+        if (this.chartData[i] instanceof Object) {
+          labels[i] = this.chartData[i].label;
+        }
+      }
+      return labels;
     },
     values () {
       let values = [];
-      for (let i = 0; i < this.chartData.length; i++) {
-        values[i] = this.chartData[i] * this.center / this.maxValue;
+      for (let i = 0; i < this.theData.length; i++) {
+        values[i] = this.theData[i] * this.center / this.maxValue;
       }
       return values;
+    },
+    textsPosition () {
+      let textsPos = [];
+      for (let i = 0; i < this.theData.length; i++) {
+        textsPos.push([
+          (this.coords[i][1][0] + this.coords[i][2][0] + this.coords[i][0][0]) / 3,
+          (this.coords[i][1][1] + this.coords[i][2][1] + this.coords[i][0][1]) / 3
+        ]);
+      }
+      return textsPos;
     },
     theOptions () {
       return !this.options ? {} : this.options;
@@ -98,6 +243,53 @@ export default {
       }
       return coords;
     },
+    guideLines () {
+      let coords = [];
+
+      let coord = [];
+      for (let i = 0; i < this.itemCount; i++) {
+        coord.push(this.getCoord(this.center, i * this.angle));
+      }
+      coord.push(this.getCoord(this.center, 0));
+      coords.push(coord);
+
+      let step = parseInt(this.center / 3);
+      for (let i = 1; i >= 0; i--) {
+        let coord = [];
+        for (let j = 0; j < this.itemCount; j++) {
+          coord.push(this.getCoord(step * (i + 1), j * this.angle));
+        }
+        coord.push(this.getCoord(step * (i + 1), 0));
+        coords.push(coord);
+      }
+      
+      return coords;
+    },
+    guideValues () {
+      let values = [];
+
+      let coord1 = this.getCoord(this.center, 0);
+      let coord2 = this.getCoord(this.center, this.angle);
+      values.push({
+        id: 0,
+        value: this.maxValue,
+        coord: [(coord1[0] + coord2[0]) / 2 - 17, (coord1[1] + coord2[1]) / 2]
+      });
+
+      let step = parseInt(this.maxValue / 3);
+      let coordStep = parseInt(this.center / 3);
+      for (let i = 1, j = 1; i >= 0, j < 3; i--, j++) {
+        coord1 = this.getCoord(coordStep * (i + 1), 0);
+        coord2 = this.getCoord(coordStep * (i + 1), this.angle);
+        values.push({
+          id: j,
+          value: step * (i + 1),
+          coord: [(coord1[0] + coord2[0]) / 2, (coord1[1] + coord2[1]) / 2]
+        });
+      }
+
+      return values;
+    },
     itemCount () {
       return this.chartData.length;
     },
@@ -106,7 +298,9 @@ export default {
       let fillsIsArray = Array.isArray(this.theOptions.fills || null);
       let fillsIsString = this.theOptions.fills ? typeof this.theOptions.fills === 'string' : false;
       for (let i = 0; i < this.itemCount; i++) {
-        if (fillsIsString) {
+        if (this.chartData[i] instanceof Object && this.chartData[i].fill) {
+          colors.push(this.chartData[i].fill);
+        } else if (fillsIsString) {
           colors.push(this.theOptions.fills);
         } else if (fillsIsArray && this.theOptions.fills[i]) {
           colors.push(this.theOptions.fills[i]);
@@ -121,7 +315,9 @@ export default {
       let fillsIsArray = Array.isArray(this.theOptions.hoverFills || null);
       let fillsIsString = this.theOptions.hoverFills ? typeof this.theOptions.hoverFills === 'string' : false;
       for (let i = 0; i < this.itemCount; i++) {
-        if (fillsIsString) {
+        if (this.chartData[i] instanceof Object && this.chartData[i].hover) {
+          colors.push(this.chartData[i].hover);
+        } else if (fillsIsString) {
           colors.push(this.theOptions.hoverFills);
         } else if (fillsIsArray && this.theOptions.hoverFills[i]) {
           colors.push(this.theOptions.hoverFills[i]);
@@ -130,6 +326,18 @@ export default {
         }
       }
       return colors;
+    },
+    tooltipData () {
+      let tooltip = [];
+      let tooltipStr = this.theOptions.tooltip || this.default.tooltip;
+      for (let i = 0; i < this.itemCount; i++) {
+        tooltip.push(
+          tooltipStr
+            .replace('${label}', this.labelData[i])
+            .replace('${value}', this.getFormattedValue(this.theData[i]))
+        );
+      }
+      return tooltip;
     }
   },
   methods: {
@@ -161,6 +369,12 @@ export default {
       }
       this.$emit('onItemClick', index);
     },
+    onGuideLineMouseOver () {
+      this.showGuideValue = true;
+    },
+    onGuideLineMouseOut () {
+      this.showGuideValue = false;
+    },
     setSelected (index) {
       this.selected = index;
     },
@@ -172,6 +386,13 @@ export default {
     },
     getRandomColor () {
       return '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1,6);
+    },
+    getFormattedValue (value) {
+      let frmValue = numeral(value);
+      if (this.numberFormat) {
+        return frmValue.format(this.numberFormat);
+      }
+      return value;
     }
   },
   mounted () {
@@ -179,3 +400,40 @@ export default {
   }
 };
 </script>
+<style scoped>
+.tsc-container {
+  padding: 16px;
+}
+.tsc-legends {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+}
+.tsc-legend {
+  margin: 16px 6px 6px;
+}
+.tsc-shape {
+  border: 1px solid #565656;
+  display: inline-block;
+  width: 20px;
+  height: 16px;
+  line-height: 16px;
+}
+.tsc-title {
+  margin-left: 4px;
+  line-height: 16px;
+  display: inline-block;
+  vertical-align: text-top;
+  font-size: 1rem;
+}
+.tsc-guide-line {
+  stroke: #656565;
+  stroke-width: 1;
+  stroke-dasharray: 5;
+  fill: transparent;
+  opacity: 0.3;
+  pointer-events: visibleStroke
+}
+</style>
